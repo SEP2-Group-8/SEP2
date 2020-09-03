d951156e9928404a650ebdc54c90a95452d5a8a9
lks1909 <lk.saldanha@gmail.com>
2020-09-03 11:07:03 +0200

    test 2

--- src/dk/via/sep/Main.java
+++ src/dk/via/sep/Main.java
@@ -3,6 +3,6 @@ package dk.via.sep;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is just a small test");
+        System.out.println("This is just a small test number 2");
     }
 }
