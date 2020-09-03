f08063cb2132f77464632c507b9352f58f403524
Mihail Gabriel <alltaircs@gmail.com>
2020-09-03 11:04:40 +0200

    Update Main.java

--- src/dk/via/sep/Main.java
+++ src/dk/via/sep/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is just a small test");
         System.out.println("this is a second test");
+        //gdfgfdg
 
 
     }
