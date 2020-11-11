ce1c29935bac241180b705f6b4dd9ce38c4b6027
weilderbeast <nu_imi_pasa_01>
2020-11-11 21:23:57 +0100

    Tested github integration.

--- src/dk/via/sep/Main.java
+++ src/dk/via/sep/Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.println("This is just a small test");
         System.out.println("this is a second test");
         System.out.println("Add something else");
-
-
+        System.out.println("another thing");
     }
 }
