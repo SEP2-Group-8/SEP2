5db2a1468b483dec820da55d483943345e45903c
Yuhao-1999 <wyh19990829@sina.com>
2020-11-11 21:37:46 +0100

    test

--- src/dk/via/sep/Main.java
+++ src/dk/via/sep/Main.java
@@ -3,9 +3,15 @@ package dk.via.sep;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.printLn("test");
         System.out.println("This is just a small test");
         System.out.println("this is a second test");
         System.out.println("Add something else");
         System.out.println("another thing");
     }
+
 }
+/*test
+* ehuhh*/
+
+
