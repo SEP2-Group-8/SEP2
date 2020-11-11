78c444201d7b55d6417c84c6aa03936679ec1500
Mihail Gabriel <alltaircs@gmail.com>
2020-11-11 21:40:42 +0100

    test

--- src/dk/via/sep/Main.java
+++ src/dk/via/sep/Main.java
@@ -3,7 +3,7 @@ package dk.via.sep;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printLn("test");
+        System.out.println("test");
         System.out.println("This is just a small test");
         System.out.println("this is a second test");
         System.out.println("Add something else");
