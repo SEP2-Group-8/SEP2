c2408a9f3b632f4325ad4684333207a7cd133761
lks1909 <lk.saldanha@gmail.com>
2020-11-18 17:59:49 +0100

    client + runclient classes

--- src/dk/via/sep/client/Client.java
+++ src/dk/via/sep/client/Client.java
@@ -1,4 +1,20 @@
 package dk.via.sep.client;
 
-public class Client {
+import dk.via.sep.client.core.ClientFactory;
+import dk.via.sep.client.core.ModelFactory;
+import dk.via.sep.client.core.ViewHandler;
+import dk.via.sep.client.core.ViewModelFactory;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+
+  @Override 
+  public void start(Stage stage) throws Exception {
+    ClientFactory clientFactory = new ClientFactory();
+    ModelFactory modelFactory = new ModelFactory(clientFactory);
+    ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
+    ViewHandler viewHandler = new ViewHandler(viewModelFactory);
+    viewHandler.start();
+  }
 }
--- src/dk/via/sep/client/RunClient.java
+++ src/dk/via/sep/client/RunClient.java
@@ -1,4 +1,9 @@
 package dk.via.sep.client;
 
+import javafx.application.Application;
+
 public class RunClient {
+  public static void main(String[] args) {
+    Application.launch(Client.class);
+  }
 }
