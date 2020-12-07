cdc74794ce46f5515bf9c9b9a7639a733df3f6d7
Mihail Gabriel <alltaircs@gmail.com>
2020-12-07 20:29:07 +0100

    Edit Event functionality

--- src/dk/via/sep/client/view/adminEditEvent/AdminEditEventViewModel.java
+++ src/dk/via/sep/client/view/adminEditEvent/AdminEditEventViewModel.java
@@ -90,36 +90,46 @@ public class AdminEditEventViewModel {
 
     public void saveEventChanges() {
 
-        Date date = new Date(eventDate.getValue().atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli());
-        Time startTime = Time.valueOf(eventTime.get());
-        Time departStartTime = Time.valueOf(busDepartLocationStartTime.get());
-        Time departEndTime = Time.valueOf(busDepartLocationEndTime.get());
-        Time arriveStartTime = Time.valueOf(busArriveLocationStartTime.get());
-        Time arriveEndTime = Time.valueOf(busArriveLocationEndTime.get());
-        int noOfSeats = Integer.parseInt(busSeats.getValue());
-
-        if(eventName.getValue().equals("") || eventName.getValue() == null)
+        if(!(eventName.getValue().equals("") || eventName.getValue() == null))
         { LoggedUser.getInstance().getSelectedEvent().setEventName(eventName.getValue()); }
-        if(eventDate.get() == null)
-        { LoggedUser.getInstance().getSelectedEvent().setDate(date);}
-        if(eventTime.get() == null)
-        { LoggedUser.getInstance().getSelectedEvent().setStartTime(startTime);}
-        if(eventDescription.getValue().equals("") || eventDescription.getValue() == null)
+        if(!(eventDate.get() == null)) {
+            Date date = new Date(eventDate.getValue().atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli());
+            LoggedUser.getInstance().getSelectedEvent().setDate(date);}
+        if(!(eventTime.get() == null))
+        {
+            Time startTime = Time.valueOf(eventTime.get());
+            LoggedUser.getInstance().getSelectedEvent().setStartTime(startTime);
+        }
+        if(!(eventDescription.getValue().equals("") || eventDescription.getValue() == null))
         { LoggedUser.getInstance().getSelectedEvent().setEventName(eventName.getValue()); }
-        if(eventLocation.getValue().equals("") || eventLocation.getValue() == null)
-        { LoggedUser.getInstance().getSelectedEvent().setLocation(eventLocation.getValue()); }
-        if(busDepartLocation.getValue().equals("") || busDepartLocation.getValue() == null)
+        if(!(eventLocation.getValue().equals("") || eventLocation.getValue() == null))
+        {LoggedUser.getInstance().getSelectedEvent().setLocation(eventLocation.getValue()); }
+        if(!(busDepartLocation.getValue().equals("") || busDepartLocation.getValue() == null))
         { LoggedUser.getInstance().getSelectedEvent().getBus().setDepartLocation(busDepartLocation.getValue()); }
-        if(busDepartLocationStartTime.get() == null)
-        { LoggedUser.getInstance().getSelectedEvent().getBus().setDepartTimeStart(departStartTime); }
-        if(busDepartLocationEndTime.get() == null)
-        { LoggedUser.getInstance().getSelectedEvent().getBus().setDepartTimeEnd(departEndTime); }
-        if(busArriveLocationStartTime.get() == null)
-        { LoggedUser.getInstance().getSelectedEvent().getBus().setArriveTimeStart(arriveStartTime); }
-        if(busArriveLocationEndTime.get() == null)
-        { LoggedUser.getInstance().getSelectedEvent().getBus().setArriveTimeEnd(arriveEndTime); }
-        if(busSeats.getValue().equals("") || busSeats.getValue() == null)
-        { LoggedUser.getInstance().getSelectedEvent().getBus().setNoSeats(noOfSeats); }
+        if(!(busDepartLocationStartTime.get() == null))
+        {
+            Time departStartTime = Time.valueOf(busDepartLocationStartTime.get());
+            LoggedUser.getInstance().getSelectedEvent().getBus().setDepartTimeStart(departStartTime);
+        }
+        if(!(busDepartLocationEndTime.get() == null))
+        {
+            Time departEndTime = Time.valueOf(busDepartLocationEndTime.get());
+            LoggedUser.getInstance().getSelectedEvent().getBus().setDepartTimeEnd(departEndTime);
+        }
+        if(!(busArriveLocationStartTime.get() == null))
+        {
+            Time arriveStartTime = Time.valueOf(busArriveLocationStartTime.get());
+            LoggedUser.getInstance().getSelectedEvent().getBus().setArriveTimeStart(arriveStartTime);
+        }
+        if(!(busArriveLocationEndTime.get() == null))
+        {
+            Time arriveEndTime = Time.valueOf(busArriveLocationEndTime.get());
+            LoggedUser.getInstance().getSelectedEvent().getBus().setArriveTimeEnd(arriveEndTime); }
+        if(!(busSeats.getValue().equals("") || busSeats.getValue() == null))
+        {
+            int noOfSeats = Integer.parseInt(busSeats.getValue());
+            LoggedUser.getInstance().getSelectedEvent().getBus().setNoSeats(noOfSeats);
+        }
 
         eventModel.editEvent(LoggedUser.getInstance().getSelectedEvent());
     }
